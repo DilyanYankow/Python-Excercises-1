63ee9f7f7f813d5c85cc27cc37a3880b44ffc02d
DilyanYankow <xdidox3@abv.bg>
2021-08-17 09:43:13 +0300

    17.08.21

--- modules.py
+++ modules.py
@@ -1,2 +1,8 @@
 # module = a file containing python code. May contain functions, classes, ect.
 # used with modular programming, which is to separate a program into parts
+
+import modules_messages as mmsg
+# from functions import name        #only imports 1 or few or all(import *) function
+help("modules")             #gives a list of modules you can import
+mmsg.hello()
+mmsg.bye()
